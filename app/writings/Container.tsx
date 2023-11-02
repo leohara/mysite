64dd9bdad991832ad842d83e4ec3be8a4183c569
leohara <engbeatleos@gmail.com>
2023-11-02 09:22:37 +0900

    ルーティングの設定

--- app/writings/Container.tsx
+++ app/writings/Container.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { useState } from "react";
+import { usePathname } from "next/navigation";
 import Header from "../components/Header";
+import Link from "next/link";
 
 type Writing = {
   id: string;
@@ -16,6 +18,10 @@ type Writing = {
 export default function Container({ writings }: { writings: Writing[] }) {
   const [hasScrolled, setHasScrolled] = useState(false);
 
+  const pathName = usePathname()
+    .split("/")
+    .filter((segment) => segment != "")[1];
+
   const onScroll = (e: React.UIEvent<HTMLDivElement>) => {
     setHasScrolled(e.currentTarget.scrollTop > 0);
   };
@@ -29,13 +35,19 @@ export default function Container({ writings }: { writings: Writing[] }) {
       <div className="pt-[80px]">
         <div className="flex-col px-[24px]">
           {writings.map((writing: Writing) => (
-            <div key={writing.id} className="mb-[8px] flex-col">
-              <p className="text-[14px] font-bold">{writing.title}</p>
-              <p className="text-[14px]">{writing.content}</p>
-              <p className="text-[12px] text-[#9f9f9f]">
-                {new Date(writing.updatedAt).toISOString()}
-              </p>
-            </div>
+            <Link href={`/writings/${writing.postId}`} key={writing.id}>
+              <div
+                className={`
+                ${"mb-[8px] flex-col rounded-lg p-[6px] px-[10px] hover:bg-[#C7FBEC]"}
+                ${pathName == writing.postId ? "!bg-[aquamarine]" : ""}
+                `}
+              >
+                <p className="text-[14px] font-bold">{writing.title}</p>
+                <p className="text-[12px] text-[#9f9f9f]">
+                  {new Date(writing.updatedAt).toISOString()}
+                </p>
+              </div>
+            </Link>
           ))}
         </div>
       </div>
