631709f10e0284c07c56fe0e8021a627c1890e81
leohara <engbeatleos@gmail.com>
2023-11-11 15:47:39 +0900

    animationの追加

--- app/about/Container.tsx
+++ app/about/Container.tsx
@@ -4,6 +4,20 @@ import Highlight from "./Highlight";
 import Stack from "./Stack";
 import Favorite from "./Favorite";
 
+function List({ experience }: { experience: boolean }) {
+  return (
+    <p
+      className={`
+    ${"mr-[4px] font-bold"}
+    ${experience ? "text-[#6081a2]" : "text-[#5a9a92]"}
+    `}
+    >
+      {" "}
+      -{" "}
+    </p>
+  );
+}
+
 export default function Container() {
   return (
     <div>
@@ -29,7 +43,8 @@ export default function Container() {
             experience={true}
           >
             <Highlight experience={true}>
-              <li>
+              <li className="flex">
+                <List experience={true} />
                 Developed a program using Python to automatically generate
                 AutoCAD drawings (.scr).
               </li>
@@ -43,11 +58,13 @@ export default function Container() {
             experience={true}
           >
             <Highlight experience={true}>
-              <li>
+              <li className="flex">
+                <List experience={true} />
                 Developed a document classification model using natural language
                 processing with Transformers.
               </li>
-              <li>
+              <li className="flex">
+                <List experience={true} />
                 Developed an application using ChatGPT&apos;s API to extract
                 specific items from documents.
               </li>
@@ -67,7 +84,10 @@ export default function Container() {
             experience={false}
           >
             <Highlight experience={false}>
-              <li>Planning to pursue a master&apos;s degree.</li>
+              <li className="flex">
+                <List experience={false} />
+                Planning to pursue a master&apos;s degree.
+              </li>
             </Highlight>
           </Experience>
           <Experience
@@ -78,16 +98,19 @@ export default function Container() {
             experience={false}
           >
             <Highlight experience={false}>
-              <li>
+              <li className="flex">
+                <List experience={false} />
                 Studied four mechanics (materials, thermal, fluids, mechanical),
                 control engineering, aerospace engineering, and programming.
               </li>
-              <li>
+              <li className="flex">
+                <List experience={false} />
                 Member of a lab specializing in computer architecture, focusing
                 on research in video image compression efficiency, especially in
                 reducing computational load in inter-frame prediction.
               </li>
-              <li>
+              <li className="flex">
+                <List experience={false} />
                 Served as the chief of wings for a human-powered airplane. Won
                 the 44th Birdman Rally (鳥人間コンテスト) in the Human-powered
                 Propeller Aircraft Category.
