9c7c4fe576c6ccd0c2101e5122906f8f9647fdf9
leohara <engbeatleos@gmail.com>
2023-11-14 03:30:23 +0900

    ログインの追加

--- app/(admin)/control-panel/layout.tsx
+++ app/(admin)/control-panel/layout.tsx
@@ -1,26 +1,32 @@
-/* eslint-disable tailwindcss/no-custom-classname */
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { redirect } from "next/navigation";
+import NextAuthSessionProvider from "@/app/provider/SessionProvider";
 import "./globals.css";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/app/utils/authOptions";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "edit page",
   description: "beatleos",
 };
 
-const inter = Inter({ subsets: ["latin"] });
-
 export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession(authOptions);
+  if (!session) redirect("/api/auth/signin");
+
   return (
     <html lang="en">
       <body
         className={`${inter.className} m-0 box-border h-screen w-full overflow-x-hidden p-0`}
       >
-        {children}
+        <NextAuthSessionProvider>{children}</NextAuthSessionProvider>
       </body>
     </html>
   );
