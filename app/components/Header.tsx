ddd10bb2af7dec3cc74b42b2636c168f31dd2431
leohara <engbeatleos@gmail.com>
2023-11-16 01:08:19 +0900

    ハンバーガーメニューの位置修正

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import { AiOutlineClose } from "react-icons/ai";
 import { SidebarContext } from "../provider/SidebarProvider";
 
 type Props = {
@@ -8,7 +9,7 @@ type Props = {
 };
 
 export default function Header({ title, hasScrolled, position }: Props) {
-  const { isOpen } = useContext(SidebarContext);
+  const { isOpen, setIsOpen } = useContext(SidebarContext);
 
   return (
     <div
@@ -16,7 +17,7 @@ export default function Header({ title, hasScrolled, position }: Props) {
         ${"z-20 h-[48px] overflow-hidden"}
         ${
           position == "left"
-            ? "grid w-[300px] grid-cols-[0fr] bg-[#f6f6f6] lg:w-[200px] lg:grid-cols-[1fr]"
+            ? "grid w-[320px] grid-cols-[0fr] bg-[#f6f6f6] lg:w-[200px] lg:grid-cols-[1fr]"
             : ""
         }
         ${
@@ -34,18 +35,48 @@ export default function Header({ title, hasScrolled, position }: Props) {
             ? "fixed w-screen border-r-[0.5px]  border-r-[#eeeff2] bg-[#fff] lg:w-[calc(100vw-520px)]"
             : ""
         }
-        ${isOpen && position !== "left" ? "bg-[#ccc] opacity-5" : ""}
-        ${hasScrolled ? "border-b shadow-bottom" : "shadow-none"}
+        ${isOpen && position !== "left" ? "bg-[#ccc]" : ""}
+        ${hasScrolled ? "border-b opacity-80 shadow-bottom" : "shadow-none"}
         transition-shadow duration-300
         `}
     >
-      <div className="w-full px-[25px] pb-[14px] pt-[16px]">
+      <div className="flex w-full px-[25px] pb-[14px] pt-[16px]">
+        <button onClick={() => setIsOpen(!isOpen)}>
+          {isOpen ? (
+            <div
+              className={`
+              ${"mt-[-2px] h-[25px] w-[25px] rounded-[50%] bg-[#f6f6f6] pl-[4px] pt-[4px]"}
+              ${
+                hasScrolled
+                  ? "border-[0.5px] border-[#eeeff2] pl-[3.5px] pt-[3.5px]"
+                  : ""
+              }
+              ${isOpen && position !== "left" ? "opacity-0" : ""}
+              `}
+            >
+              <AiOutlineClose size={18} />
+            </div>
+          ) : (
+            <div
+              className={`
+              ${"space-y-[4px]"}
+              ${position == "left" ? "hidden" : "lg:hidden"}
+              ${position == "right-detail" ? "hidden" : ""}
+              `}
+            >
+              <div className="h-[2px] w-4 bg-[#404040]"></div>
+              <div className="h-[2px] w-4 bg-[#404040]"></div>
+              <div className="h-[2px] w-4 bg-[#404040]"></div>
+            </div>
+          )}
+        </button>
         <p
           className={`
-          ${"font-[#363636] font-bold"}
-          ${position == "left" ? "pl-[24px]" : "pl-[32px] lg:pl-[0px]"}
-          ${position == "right-detail" ? "truncate" : ""}
-        `}
+            ${"font-[#363636] font-bold"}
+            ${position == "left" ? "" : "pl-[24px] lg:pl-[0px]"}
+            ${isOpen ? "pl-[24px]" : ""}
+            ${position == "right-detail" ? "ml-[12px] truncate" : ""}
+          `}
         >
           {title}
         </p>
