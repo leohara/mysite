66f4bec5befd0d4e938a2b8fdd1ff2cc0e44d7cf
leohara <engbeatleos@gmail.com>
2023-11-25 05:23:04 +0900

    Add OGP

--- app/(general)/layout.tsx
+++ app/(general)/layout.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable tailwindcss/no-custom-classname */
 import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
+import { SITE_NAME, SITE_URL } from "@/app/constants/site";
 
 import Container from "./Container";
 
@@ -13,8 +14,21 @@ import "@/app/globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "beatleos",
-  description: "beatleos",
+  metadataBase: new URL(SITE_URL),
+  title: "leohara",
+  description: "leohara personal website.",
+  keywords: ["leohara", "原田零生"],
+  openGraph: {
+    type: "website",
+    locale: "ja_JP",
+    url: SITE_URL,
+    siteName: SITE_NAME,
+    title: SITE_NAME,
+    description: "leohara personal website.",
+  },
+  twitter: {
+    card: "summary_large_image",
+  },
 };
 
 export default function RootLayout({
