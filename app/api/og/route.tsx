1b9898203308e48c10cc7ce923f9ad5efdd593de
leohara <engbeatleos@gmail.com>
2023-11-25 19:59:29 +0900

    Fix edge

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -12,7 +12,7 @@ export async function GET(request: Request) {
       <div tw="flex h-full w-full flex-col bg-[#333] py-[30px] px-[200px] py-[80px]">
         <div tw="flex flex-col bg-[#f6f6f6] px-[80px] py-[30px] rounded-[16px]">
           <div tw="flex flex-col h-[350px]">
-            <p tw="text-[48px] font-bold text-[#404040]">test</p>
+            <p tw="text-[48px] font-bold text-[#404040]">{postId}</p>
           </div>
           <div tw="flex justify-center">
             <img
