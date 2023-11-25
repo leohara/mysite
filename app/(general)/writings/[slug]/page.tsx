7f9db563bb5973c48b69c1aa774a6235344d774e
leohara <engbeatleos@gmail.com>
2023-11-25 11:10:00 +0900

    Add og route

--- app/(general)/writings/[slug]/page.tsx
+++ app/(general)/writings/[slug]/page.tsx
@@ -1,13 +1,46 @@
 import { Suspense } from "react";
+import { ImageResponse } from 'next/og'
+
+import { Metadata } from "next";
 
 import Container from "./Container";
 import Post from "./_presenter/Post";
 
-export default async function Page({ params }: { params: { slug: string } }) {
+import { prisma } from "@/app/lib/db/prisma";
+
+type ListingPageProps = {
+  params: {
+    id: string;
+  };
+};
+
+export async function generateMetadata({
+  params: { id },
+}: ListingPageProps): Promise<Metadata> {
+  const writing = await prisma.writing.findUnique({
+    where: { postId: id },
+  });
+
+  return {
+    title: writing?.title,
+    description: writing?.content,
+    openGraph: {
+      title: writing?.title,
+      description: writing?.content,
+      images: {
+        url: "",
+        width: 1200,
+        height: 630,
+      },
+    },
+  };
+}
+
+export default async function Page({ params: { id } }: ListingPageProps) {
   return (
     <Container>
       <Suspense fallback={<p className="text-center">Loading...</p>}>
-        <Post postId={params.slug} />
+        <Post postId={id} />
       </Suspense>
     </Container>
   );
