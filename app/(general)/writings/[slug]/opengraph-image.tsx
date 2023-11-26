3b95f477e8e7aebe8e0fceb3d433467b4c8f9e09
leohara <engbeatleos@gmail.com>
2023-11-26 10:10:03 +0900

    Fix metadata

--- app/(general)/writings/[slug]/opengraph-image.tsx
+++ app/(general)/writings/[slug]/opengraph-image.tsx
@@ -13,6 +13,7 @@ export const size = {
   width: 1200,
   height: 630,
 };
+export const description = "detail page of leohara";
 
 export const contentType = "image/png";
 
--- app/(general)/writings/[slug]/twitter-image.tsx
+++ app/(general)/writings/[slug]/twitter-image.tsx
@@ -13,6 +13,7 @@ export const size = {
   width: 1200,
   height: 630,
 };
+export const description = "detail page of leohara in twitter";
 
 export const contentType = "image/png";
 
