fbbcad8dbe071c1662acfab6e775892f77814ea6
leohara <engbeatleos@gmail.com>
2023-11-29 23:19:40 +0900

    Add ttl

--- app/lib/db/prisma.ts
+++ app/lib/db/prisma.ts
@@ -1,11 +1,4 @@
 import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-import { env } from "../env";
-
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
-
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
-
-if (env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+export const prisma = new PrismaClient().$extends(withAccelerate());
