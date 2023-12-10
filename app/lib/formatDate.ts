8df9cbe094fd24a7e8d74de3a873bd8ee649731e
leohara <engbeatleos@gmail.com>
2023-12-11 03:55:24 +0900

    fix formatDate

--- app/lib/formatDate.ts
+++ app/lib/formatDate.ts
@@ -1,5 +1,4 @@
 export function formatDate(dateUTC: Date | null): string {
-
   if (!dateUTC) return "";
 
   const date = new Date(dateUTC);
@@ -8,8 +7,8 @@ export function formatDate(dateUTC: Date | null): string {
   date.setMinutes(date.getMinutes() + jstOffset);
 
   const year = date.getFullYear();
-  const month = (date.getMonth() + 1).toString().padStart(2, '0');
-  const day = date.getDate().toString().padStart(2, '0');
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
 
   return `${year}-${month}-${day}`;
 }
